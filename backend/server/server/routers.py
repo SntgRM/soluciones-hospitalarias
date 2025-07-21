196e9bee95a9c4e6732031268053240c5335c11e
samirdp04 <samirdelportillo01@gmail.com>
2025-07-21 16:26:45 -0500

    UPD - Improvements in the router code by implementing the MultiDBRouter and the new warehouse database.

--- backend/server/server/routers.py
+++ backend/server/server/routers.py
@@ -1,5 +1,6 @@
 # Este nombre debe coincidir con el que usas en DATABASES
-USERS_DB = 'default'  # porque 'default' apunta a 'sh_usuarios'
+USERS_DB = 'default' 
+BODEGA_DB = 'bodega' 
 
 USERS_MODELS = [
     'auth',
@@ -9,15 +10,22 @@ USERS_MODELS = [
     'authtoken',
 ]
 
-class AuthRouter:
+USERS_MODELS = ['auth', 'admin', 'contenttypes', 'sessions', 'authtoken']
+BODEGA_MODELS = ['warehouse']
+
+class MultiDBRouter:
     def db_for_read(self, model, **hints):
         if model._meta.app_label in USERS_MODELS:
             return USERS_DB
+        if model._meta.app_label in BODEGA_MODELS:
+            return BODEGA_DB
         return None
 
     def db_for_write(self, model, **hints):
         if model._meta.app_label in USERS_MODELS:
             return USERS_DB
+        if model._meta.app_label in BODEGA_MODELS:
+            return BODEGA_DB
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
@@ -26,4 +34,6 @@ class AuthRouter:
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in USERS_MODELS:
             return db == USERS_DB
-        return db != USERS_DB  # Esto previene que los modelos normales se vayan a 'default' también
+        if app_label in BODEGA_MODELS:
+            return db == BODEGA_DB
+        return db == 'default'
