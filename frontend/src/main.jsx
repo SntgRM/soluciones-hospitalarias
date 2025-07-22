091e21c63269d1fdd0a2dbf9414fbfc4882d75ca
SntgRM <santy01052007@gmail.com>
2025-07-22 16:22:35 -0500

    ADD - added the browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import { SidebarProvider } from './context/sidebarContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <SidebarProvider>
-    <App />
-  </SidebarProvider>
+  <BrowserRouter>
+    <SidebarProvider>
+      <App />
+    </SidebarProvider>
+  </BrowserRouter>
 )
