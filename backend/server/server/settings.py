d1e3f576a9a8e87cef63d8e55c65d7b9a205ce2b
SntgRM <santy01052007@gmail.com>
2025-07-22 10:13:34 -0500

    UPD - updated configuration to implement 'corsheaders'

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'authentication',
     'warehouse',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -146,3 +149,11 @@ REST_FRAMEWORK = {
 }
 
 DATABASE_ROUTERS = ['server.routers.MultiDBRouter']
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
+CORS_ALLOW_CREDENTIALS = True
