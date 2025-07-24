fd5d5a0eab3e0cb273f09bdc35234ab447c44f05
samirdp04 <samirdelportillo01@gmail.com>
2025-07-24 13:40:02 -0500

    REF - Code improvements for the Urls/server and Urls/warehouse routes, adding more routes.

--- backend/server/warehouse/urls.py
+++ backend/server/warehouse/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+from .views import FacturaViewAll, FacturaDetail, FacturaCreate, FacturaUpdate, FacturaDelete
+
+urlpatterns = [
+    path('showall/', FacturaViewAll.as_view(), name='factura-view-all'),
+    path('show/<int:pk>/', FacturaDetail.as_view(), name='factura-detail'),
+    path('create/', FacturaCreate.as_view(), name='factura-create'),
+    path('update/<int:pk>/', FacturaUpdate.as_view(), name='factura-update'),
+    path('delete/<int:pk>/', FacturaDelete.as_view(), name='factura-delete'),
+    
+]
