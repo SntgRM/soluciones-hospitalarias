cdb071c3411634533028240884a69c21313da1c3
SntgRM <santy01052007@gmail.com>
2025-07-24 14:17:34 -0500

    ADD - added User model, to define the respective roles

--- backend/server/authentication/models.py
+++ backend/server/authentication/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLES = (
+        ('administrador', 'Administrador'),
+        ('bodega', 'Bodega'),
+        ('ventas', 'Ventas'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLES, default='bodega')
