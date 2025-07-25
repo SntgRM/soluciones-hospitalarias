f34945b11e5cb28eea8ac1a9310acc90a5ec7145
SntgRM <santy01052007@gmail.com>
2025-07-25 11:38:22 -0500

    UPD - updating the configuration for routes and MEDIA management

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -165,3 +166,7 @@ CORS_ALLOW_CREDENTIALS = True
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+# Media files settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
