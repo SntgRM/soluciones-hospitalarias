40fb8bbd183356fb51af289fca0727c8e4d7e67e
SntgRM <santy01052007@gmail.com>
2025-07-25 11:40:31 -0500

    REF - small improvement in the display of the Django administration panel; it is now easier to access user information from the user list.

--- backend/server/authentication/admin.py
+++ backend/server/authentication/admin.py
@@ -5,7 +5,7 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('id', 'username', 'first_name', 'role', 'is_staff')
+    list_display = ('username', 'id', 'first_name', 'role', 'is_staff')
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('role',)}),
     )
