da4499f94a7f9ce944d63bfe34e0de7f637a331d
samirdp04 <samirdelportillo01@gmail.com>
2025-07-26 08:48:02 -0500

    ADD - Added the foreign keys from the orders table to display them as their name in the JSON.

--- backend/server/warehouse/serializers.py
+++ backend/server/warehouse/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Pedidos
 
 class PedidoSerializer(serializers.ModelSerializer):
+    id_cliente = serializers.StringRelatedField()
+    id_vendedor = serializers.StringRelatedField()
+    id_transportadora = serializers.StringRelatedField()
+    id_estado = serializers.StringRelatedField()
+    id_enrutador = serializers.StringRelatedField()
+    id_alistador = serializers.StringRelatedField()
+    id_empacador = serializers.StringRelatedField()
+
     class Meta:
         model = Pedidos
         fields = '__all__'
