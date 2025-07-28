0f8ef1d334a93641502865bdbde7017331a73777
samirdp04 <samirdelportillo01@gmail.com>
2025-07-28 09:05:23 -0500

    REF - Improvement of code to display the name of foreign tables and ID in the orders table.

--- backend/server/warehouse/serializers.py
+++ backend/server/warehouse/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import Pedidos
 
 class PedidoSerializer(serializers.ModelSerializer):
-    id_cliente = serializers.StringRelatedField()
-    id_vendedor = serializers.StringRelatedField()
-    id_transportadora = serializers.StringRelatedField()
-    id_estado = serializers.StringRelatedField()
-    id_enrutador = serializers.StringRelatedField()
-    id_alistador = serializers.StringRelatedField()
-    id_empacador = serializers.StringRelatedField()
+    cliente_nombre = serializers.StringRelatedField(source='id_cliente', read_only=True)
+    vendedor_nombre = serializers.StringRelatedField(source='id_vendedor', read_only=True)
+    transportadora_nombre = serializers.StringRelatedField(source='id_transportadora', read_only=True)
+    estado_nombre = serializers.StringRelatedField(source='id_estado', read_only=True)
+    enrutador_nombre = serializers.StringRelatedField(source='id_enrutador', read_only=True)
+    alistador_nombre = serializers.StringRelatedField(source='id_alistador', read_only=True)
+    empacador_nombre = serializers.StringRelatedField(source='id_empacador', read_only=True)
 
     class Meta:
         model = Pedidos
