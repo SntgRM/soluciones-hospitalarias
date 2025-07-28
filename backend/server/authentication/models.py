84e770f46795bf21ce745705152789c180ad4744
SntgRM <santy01052007@gmail.com>
2025-07-28 13:41:12 -0500

    ADD - added user_image_path to create a custom URL for each profile image added to users and to manage image saving and deletion.

--- backend/server/authentication/models.py
+++ backend/server/authentication/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import os
+
+def user_image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'user_{instance.id}.{ext}'
+    return os.path.join('users/profile_images/', filename)
 
 class User(AbstractUser):
     ROLES = (
@@ -9,3 +15,21 @@ class User(AbstractUser):
     )
 
     role = models.CharField(max_length=20, choices=ROLES, default='bodega')
+    profile_image = models.ImageField(upload_to = user_image_path, null = True, blank = True, help_text='Imagen de perfil del usuario')
+
+    def delete(self, *args, **kwargs):
+        if self.profile_image:
+            if os.path.isfile(self.profile_image.path):
+                os.remove(self.profile_image.path)
+        super().delete(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            try:
+                old_user = User.objects.get(pk=self.pk)
+                if old_user.profile_image and old_user.profile_image != self.profile_image:
+                    if os.path.isfile(old_user.profile_image.path):
+                        os.remove(old_user.profile_image.path)
+            except User.DoesNotExist:
+                pass
+        super().save(*args, **kwargs)
